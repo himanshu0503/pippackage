846170a22a834020402ea9399b0969ede8bf2773
Himanshu Aggarwal <haggarwal@atlassian.com>
2020-11-09 20:49:07 +0530

    Renames package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='python-package',
+    name='pippackage',
     version='0.0.1',
     description='My package from github repo',
     url='git@github.com:himanshu0503/pippackage.git',
     author='Himanshu Aggarwal',
     author_email='',
     license='unlicense',
-    packages=['python-package'],
+    packages=['pippackage'],
     zip_safe=False
 )
