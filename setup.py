1de783655a2a769496925fcadf4d60163144ed2c
Himanshu Aggarwal <haggarwal@atlassian.com>
2020-11-09 19:32:18 +0530

    Adds Hello World method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-package',
+    version='0.0.1',
+    description='My package from github repo',
+    url='git@github.com:himanshu0503/python-package.git',
+    author='Himanshu Aggarwal',
+    author_email='',
+    license='unlicense',
+    packages=['python-package'],
+    zip_safe=False
+)
